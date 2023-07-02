a59f4852b002892496ee4f5413818999256c3b5b
Niks Gont <88322931+niksgont@users.noreply.github.com>
2023-07-02 21:39:25 +0300

    Update SignUpPage.js

--- src/Components/SignIn/SignUpPage.js
+++ src/Components/SignIn/SignUpPage.js
@@ -1,11 +1,14 @@
-import "./../../assets/css/signinpage.css"
+import "./../../assets/css/signuppage.css"
 import Header from "../Header/Header2";
 import Footer from "../Footer/Footer";
-
+import SignUp from "./SignUp"
 const SignUpPage = () => {
     return (
         <div>
             <Header />
+            <div className="signunpage-aligner">
+                <SignUp /> 
+            </div>
             <Footer />
         </div>
     );
