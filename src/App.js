b9a0e1d470a8c28daa25f5156a753c3bdcfcd8fc
RoukayaZaki <roka.zaki2002@gmail.com>
2023-07-15 15:18:06 +0300

    New SignIn UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ItemPage from './Components/ItemPage/ItemPage';
 import PurchasePage from './Components/PurchasePage/PurchasePage';
 import StorePage from './Components/StorePage/StorePage';
-import SignInPage from './Components/Authentication/SignInPage';
+import SignInPage from './Components/SignIn/SignInPage';
 import SignUpPage from './Components/Authentication/SignUpPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserProfilePage from './Components/User/UserProfilePage';
