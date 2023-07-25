51914abf93cdd969673836fe8aef0cb2974cf203
RoukayaZaki <roka.zaki2002@gmail.com>
2023-07-25 10:45:18 +0300

    adding about

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -15,8 +15,7 @@ function About() {
       <div className="about">
         <h1>About Page</h1>
         <p>
-          Welcome to our About page. Here, we share information about our
-          company.
+        Welcome to our store! We are a passionate group of students from Innopolis University, united by our shared vision to bring the university community closer together through our exclusive merchandise. As fellow students, we understand the pride and spirit that courses through the veins of every member of this esteemed institution. Our mission is to create a hub where you can find unique and high-quality products that not only represent our beloved university but also embody the essence of our collective journey here. With creativity and innovation as our driving forces, we have meticulously designed each item to showcase our love for Innopolis and its vibrant culture. Every purchase from our store not only reflects your support for our initiative but also contributes to various university initiatives and causes close to our hearts. Join us on this exciting venture as we continue to grow, evolve, and leave a lasting mark on the Innopolis University legacy!
         </p>
       </div>
       <Footer />
